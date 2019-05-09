4a91d84c37a2b9dad283ffc4194c28cf403f55cf
angelalrae <angelarae512@gmail.com>
2019-05-09 01:21:45 -0700

    updated naive bayes

--- naivebayes.py
+++ naivebayes.py
@@ -2,6 +2,7 @@ import utils
 import tabulate
 import numpy as np 
 from copy import deepcopy
+import math
 
 def main():
     table = utils.read_table("StudentsPerformance.csv")
@@ -9,18 +10,13 @@ def main():
     
     clean_data(table, header)
     
-    # split data
-    test_set = utils.get_test_set(table, int(len(table) /3))
-    train_set = utils.get_training_set(table, test_set)
-    
     # make prediction about math score class
-    class_label = "writing score class"
+    class_labels = ["writing score class", "reading score class", "math score class"]
     attributes = ['gender', 'race/ethnicity', 'parental level of education', 'lunch', 'test preparation course']
-#     for test_instance in test_set:
-#         prediction = classify_using_naive_bayes(train_set, header, test_instance, class_label, attributes)
-        # print("prediction:", prediction, "actual:", test_instance[header.index("math score class")])
-
-    subsampling_accuracy(table, header, 3, class_label, attributes)
+  
+    for i, label in enumerate(["Writing", "Reading", "Math"]):
+        k = 100
+        subsampling_accuracy(table, header, k, class_labels[i], attributes, label)
 
 def clean_data(table, header):
     '''
@@ -30,9 +26,9 @@ def clean_data(table, header):
     strip_quotation_marks_table(table)
     strip_quotation_marks_list(header)
     scores_to_numeric(table, header)
-    add_categorical_scores(table, header, "math score")
-    add_categorical_scores(table, header, "reading score")
-    add_categorical_scores(table, header, "writing score")
+    add_grades(table, header, "math score")
+    add_grades(table, header, "reading score")
+    add_grades(table, header, "writing score")
 
 def strip_quotation_marks_table(table):
     '''
@@ -64,25 +60,69 @@ def scores_to_numeric(table, header):
         row[reading_index] = int(row[reading_index])
         row[writing_index] = int(row[writing_index])
 
-def add_categorical_scores(table, header, score_type):
-    '''
-    since we will be performing Naive Bayes classification, we need categorical classes
-    appends rows to table with categorical values 1-10
-    '''
-    # use equal width bins to set categories
-    score_index = header.index(score_type)
-    scores = utils.get_column(table, score_index)
-    cutoffs = utils.compute_equal_widths_cutoffs(scores, 10)
-
+def add_grades(table, header, score_type):
     # add new column to header
+    score_index = header.index(score_type)
     header.append(score_type + " class")
 
-    # for each row, find the right cutoff point and append int as class
+     # for each row, add grade from
     for row in table:
-        cutoff_counter = 0
-        while row[score_index] > cutoffs[cutoff_counter]:
-            cutoff_counter += 1
-        row.append(cutoff_counter)
+        grade = get_grade(row[score_index])
+        row.append(grade)
+
+def get_grade(score):
+    if score >= 90:
+        return "A"
+    elif score >= 80:
+        return "B"
+    elif score >= 70:
+        return "C"
+    elif score >= 60:
+        return "D"
+    else:
+        return "F"
+
+def subsampling_accuracy(table, header, k, class_label, attr_list, label):
+    '''
+    tests the accuracy of Naive Bayes classifier using random subsampling to create test sets
+    '''
+    accuracies = []
+    
+    # For k trials, partition data into train and test then classify using Naive Bayes
+    # Then print confusion matrix and compute accuracy and error rate for both sets of predictions
+    for _ in range(k):
+        # split table into train and test sets
+        train, test = utils.compute_holdout_partitions(table)
+
+        instance_accuracy = compute_naive_bayes_accuracy(train, test, header, attr_list, class_label)
+        accuracies.append(instance_accuracy)
+    
+    print("-----------------------------------------------------------------")
+    print(label, "Average Accuracy:", np.mean(accuracies))
+    print("-----------------------------------------------------------------")
+
+def compute_naive_bayes_accuracy(train, test, header, attributes_list, class_label):
+    '''
+    for a training/test set pair, computes the accuracy of the naive bayes classification
+    parameter train is the training set of data (a table) for linear regression
+    parameter test is the test set of data (a table) to classify
+    parameters table and header are the data table and a list of the attributes in order, respectively
+    returns the accuracy
+    '''
+    predictions = []
+
+    # for each instance in the training set, compute naive bayes classifications
+    for instance in test:
+        predictions.append(classify_using_naive_bayes(train, header, instance, "math score class", attributes_list))
+
+    # get actual values
+    actuals = utils.get_column(test, header.index("math score class"))
+    
+    # compute accuracy using predicted values and actuals
+    con_mat = create_confusion_matrix(predictions, actuals)
+    # print_confusion_matrix(con_mat[0], con_mat[1], con_mat[2], con_mat[3])
+
+    return acc(con_mat[0], con_mat[1], con_mat[2], con_mat[3])
 
 def classify_using_naive_bayes(table, header, test_instance, class_label, attr_list):
     '''
@@ -175,55 +215,10 @@ def compute_class_probability(prior, posteriors_list, class_index):
     
     return class_probability
 
-def subsampling_accuracy(table, header, k, class_label, attr_list):
+
+def create_confusion_matrix(predictions, actuals):
     '''
-    tests the accuracy of Naive Bayes classifier using random subsampling to create test sets
-    '''
-    accuracies = []
-    
-    # For k trials, partition data into train and test then use linear regression and kNN 
-    #    to predict for test set. Compute accuracy and error rate for both sets of predictions
-    for i in range(k):
-        # split table into train and test sets
-        train, test = utils.compute_holdout_partitions(table)
-
-        instance_accuracy = compute_naive_bayes_accuracy(train, test, header, attr_list, class_label)
-        accuracies.append(instance_accuracy)
-    
-    # compute average accuracy
-    accuracy = np.mean(accuracies)
-
-    print('\n===========================================\nPredictive Accuracy\
-    \n===========================================')
-    print("Random Subsample (k=10, 2:1 Train/Test)")
-    print("Naive Bayes: accuracy = ", round(accuracy, 2), ", error rate = ", round(1-accuracy, 2))
-
-def compute_naive_bayes_accuracy(train, test, header, attributes_list, class_label):
-    '''
-    for a training/test set pair, computes the accuracy of the naive bayes classification
-    parameter train is the training set of data (a table) for linear regression
-    parameter test is the test set of data (a table) to classify
-    parameters table and header are the data table and a list of the attributes in order, respectively
-    returns the accuracy
-    '''
-    predictions = []
-
-    # for each instance in the training set, compute naive bayes classifications
-    for instance in test:
-        predictions.append(classify_using_naive_bayes(train, header, instance, "math score class", attributes_list))
-
-    # get actual values
-    actuals = utils.get_column(test, header.index("math score class"))
-    
-    # compute accuracy using predicted values and actuals
-    accuracy = compute_accuracy(predictions, actuals)
-
-    return accuracy
-
-def compute_accuracy(predictions, actuals):
-    '''
-    given parallel lists of predicted classes and actual classes, computes the accuracy
-    returns the average of the class accuracies
+    given parallel lists of predicted classes and actual classes, creates a confusion matrix
     '''
 
     # get list of all possible classifications from predictions and actuals
@@ -231,19 +226,28 @@ def compute_accuracy(predictions, actuals):
     categories = sorted(categories)
     
     # create confusion matrix (actual vs. predicted table)
-    confusion_matrix = create_confusion_matrix(categories, actuals, predictions)
-    
-    # for each class label, compute the accuracy
-    accuracies = []
-    for class_label in categories:
-        class_index = categories.index(class_label)
-        accuracy = compute_class_accuracy(confusion_matrix, class_index)
-        accuracies.append(accuracy)
-    
-    # return the average of the class accuracies
-    return np.mean(accuracies)
+    raw_matrix = create_raw_matrix(categories, actuals, predictions)
 
-def create_confusion_matrix(categories, actuals, predictions):
+    tp = 0
+    tn = 0
+    fp = 0
+    fn = 0
+
+    # create confusion matrix with pass/fail numbers
+    for a_index,actuals_row in enumerate(raw_matrix):
+        for p_index in range(len(actuals_row)):
+            if a_index < 4 and p_index < 4:
+                tp += raw_matrix[a_index][p_index]
+            elif a_index < 4:
+                fn += raw_matrix[a_index][p_index]
+            elif p_index < 4:
+                fp += raw_matrix[a_index][p_index]
+            else:
+                tn += raw_matrix[a_index][p_index]
+
+    return (tp, tn, fp, fn)
+
+def create_raw_matrix(categories, actuals, predictions):
     '''
     creates a confusion maxtrix of actual versus predicted values
     parameter categories is a list of all unique classes
@@ -269,28 +273,41 @@ def create_confusion_matrix(categories, actuals, predictions):
         
     return confusion_matrix
 
-def compute_class_accuracy(table, class_index):
+def print_confusion_matrix(tp, tn, fp, fn):  
     '''
-    uses the binary accuracy equation (accuracy = TP + TN / P + N) to compute the accuracy of a single class
-    parameter table is the predictions versus actuals table
-    parameter class_index is an int, the index of the class to compute accuracy for
-    returns the class accuracy as a double
+        Prints a formatted confusion matrix.
     '''
-    true_positives = table[class_index][class_index]
-    
-    # compute true negatives (not classified as class and actually not in the class)
-    true_negatives = 0
-    for r_index,row in enumerate(table):
-        for v_index,value in enumerate(row):
-            if r_index != class_index and v_index != class_index:
-                true_negatives += value
+    print('\n                        Predicted       ')  
+    print('         |-------------------------------------|')
+    print('         |       |    Yes  |    No   |  Total  |')  
+    print('         |-------------------------------------|')
+    print('         |   Yes |  %5d  |  %5d  |  %5d  |' % (tp, fn, tp+fn))
+    print('  Actual |-------------------------------------|')
+    print('         |   No  |  %5d  |  %5d  |  %5d  |' % (fp, tn, fp+tn))
+    print('         |-------------------------------------|')
+    print('         | Total |  %5d  |  %5d  |  %5d  |' % (tp+fp, fn+tn, tp+tn+fp+fn))
+    print('         |-------------------------------------|\n')
+    print('              Accuracy     : %.5f' % acc(tp, tn, fp, fn))
+    print('              Standard Err : %.5f\n' % s_err(tp, tn, fp, fn))
 
-    # sum all values in table
-    total = 0
-    for row in table:
-        for value in row:
-            total += value
-    
-    return (true_positives + true_negatives) / total
+def s_err(tp, tn, fp, fn):
+    '''
+        calculate standard error
+    '''
+    correct = tp + tn
+    incorrect = fp + fn
+    ttl = correct + incorrect
+    err = math.sqrt((correct/ttl * incorrect/ttl) / ttl)
+    return err
+
+def acc(tp, tn, fp, fn):
+    '''
+        calculate accuracy (correct classifications / all classifications)
+    '''
+    correct = tp + tn
+    incorrect = fp + fn
+    ttl = correct + incorrect
+    return correct/ttl
+
 if __name__ == "__main__":
     main()
